2aca9d52dd413176a2917faf1e2433d93da99fce
Wei Lin <vlin2014@icloud.com>
2016-03-05 15:28:52 +0800

    Initial Commit

---
+++ AnimationDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDome
+//
+//  Created by LinWei on 16/3/5.
+//  Copyright © 2016年 LinWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnimationDome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimationDome
+//
+//  Created by LinWei on 16/3/5.
+//  Copyright © 2016年 LinWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
